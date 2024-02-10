135a971d6bae9cbe182de1647fa6e34c722b3bcb
Ryan Richard <tank1332@me.com>
2024-02-10 14:50:18 -0700

    switched the endpoint from using the Python3.9 lambda to using the Node v20 lambda with AWS SDK v3 syntax

--- getVisited.js
+++ getVisited.js
@@ -1,9 +1,9 @@
 // Invoking lambda to get and update the count for the amount of visitors I have had.
 
 const getVisited = async () => {
-    fetch("https://900kkpx3k1.execute-api.us-east-1.amazonaws.com/Deploy/readvisitors")
+    await fetch("https://900kkpx3k1.execute-api.us-east-1.amazonaws.com/Deploy/js-getVisited")
     .then(obj => obj.json())
-    .then(data => document.getElementById('counter').innerText = data.body.tester['N']);
+    .then(data => document.getElementById('counter').innerText = data.Item.tester['N']);
 }
 
 const addVisited = async () => {
