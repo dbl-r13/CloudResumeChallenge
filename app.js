c6320b7737e9264d30e299ce56df52e9afeffd22
Ryan Richard <tank1332@me.com>
2024-02-10 13:23:00 -0700

    refactor: renamed file from app.js to getVisited.js

--- app.js
+++
@@ -1,4 +0,0 @@
-// Invoking lambda to get and update the count for the amount of visitors I have had.
-fetch("https://900kkpx3k1.execute-api.us-east-1.amazonaws.com/Deploy/readvisitors")
-                .then(obj => obj.json())
-                .then(data => document.getElementById('counter').innerText = data.body.tester['N']);
