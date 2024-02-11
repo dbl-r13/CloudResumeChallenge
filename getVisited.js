80f1110b8cc5d449bf8f2949ea7f2a9082ef7e99
Ryan Richard <tank1332@me.com>
2024-02-10 18:42:13 -0700

    refactor: moved some logic back to index.html

--- getVisited.js
+++ getVisited.js
@@ -1,7 +1,7 @@
 // Invoking lambda to get and update the count for the amount of visitors I have had.
 
 const getVisited = async () => {
-    await fetch("https://900kkpx3k1.execute-api.us-east-1.amazonaws.com/Deploy/readvisitors")
+    await fetch("https://900kkpx3k1.execute-api.us-east-1.amazonaws.com/Deploy/js-getVisited")
     .then(obj => obj.json())
     .then(data => document.getElementById('counter').innerText = data.Item.tester['N']);
 }
@@ -11,21 +11,3 @@ const addVisited = async () => {
     .then(obj => obj.json())
     .then(data => document.getElementById('counter').innerText = data.body.tester['N']);
 }
-
-            let data = document.cookie;
-            if(!data.includes("CRC-RGR")){
-                const expirationDate = Date.now() + (86400000*2); 
-                cookieStore.set({
-                    "value": "Visitor counter that expires 2 days after site has been visited.",
-                    "expires": expirationDate,
-                    "name": "CRC-RGR"
-                });
-                console.log("Successfully added Cookie")
-                addVisited()
-                getVisited()
-
-            } else {
-                console.log("This site has been visited")
-                getVisited()
-                
-            }
